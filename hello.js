4bf3c9f57bbf9e1b3666b19368b410cea4420c6c
ponnarasi <>
2021-06-01 05:27:02 +0000

    Changed

--- hello.js
+++ hello.js
@@ -2,6 +2,7 @@ function hello() {
 	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
 	return "Hello World!";
+  console.log("Hello");
 }
  
-module.exports = hello;
+module.exports = hello; 
